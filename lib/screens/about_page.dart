efd8324c59fdcec832baa1a2af9a8d605fda0412
Ronit Rameja <120367427+master-wayne07@users.noreply.github.com>
2022-12-15 11:32:12 +0530

    Changes in about page

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -11,102 +11,90 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         leading: const BackButton(color: Colors.black),
         title: Text(
           "About Page",
           style: TextStyle(color: Colors.black),
         ),
-        backgroundColor: Color.fromARGB(255, 158, 239, 255),
+        backgroundColor: Colors.blue,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'About',
-              style: TextStyle(color: Colors.black, fontSize: 35),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Moto:',
-              style: TextStyle(color: Colors.black, fontSize: 25),
-            ),
-            Text(
-              '''The moto of our application is to provide you the live location of transport vehicles which you are concerned of. ''',
-              style: TextStyle(color: Colors.black, fontSize: 15),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Version:',
-              style: TextStyle(color: Colors.black, fontSize: 25),
-            ),
-            Text(
-              'V1.0.0',
-              style: TextStyle(color: Colors.black, fontSize: 15),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Made by:',
-              style: TextStyle(color: Colors.black, fontSize: 25),
-            ),
-            Text(
-              '''Synergy''',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-                'Team Members:-\n-Priyansh Saxena\n-Narendra Patel\n-Ronit Rameja\n-Riya Ameta\n-Vansh Arora\n-Vikas Sahu',
-                style: TextStyle(color: Colors.black, fontSize: 15)),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Feedback:',
-              style: TextStyle(color: Colors.black, fontSize: 25),
-            ),
-            // Text('priyanshsaxena40@gmail.com',
-            //     style: TextStyle(color: Colors.black, fontSize: 15)),
-            // ElevatedButton(
-            //     onPressed: () {
-            //       launch('mailto:priyanshsaxena40@gmail.com');
-            //     },
-            //     child: Text('send mail')),
-            // GestureDetector(
-            //   onTap: () {
-            //     launch('mailto:priyanshsaxena40@gmail.com');
-            //   },
-            //   child: Text('priyanshsaxena40@gmail.com'),
-            // ),
-            RichText(
+      body: Stack(children: [
+        Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Version:',
+                style: TextStyle(color: Colors.black, fontSize: 25),
+              ),
+              Text(
+                'V1.0.0',
+                style: TextStyle(color: Colors.black, fontSize: 15),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Made by:',
+                style: TextStyle(color: Colors.black, fontSize: 25),
+              ),
+              Text(
+                '''Synergy''',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                  'Team Members:-\n-Priyansh Saxena\n-Narendra Patel\n-Ronit Rameja\n-Riya Ameta\n-Vansh Arora\n-Vikas Sahu',
+                  style: TextStyle(color: Colors.black, fontSize: 15)),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Feedback:',
+                style: TextStyle(color: Colors.black, fontSize: 25),
+              ),
+              RichText(
                 text: TextSpan(
-                    text:
-                        '''Give your valuable feedbacks and suggestion on this email: ''',
-                    style: TextStyle(color: Colors.black, fontSize: 15),
-                    children: [
-                      TextSpan(
-                          text: 'synergypnrrvv@gmail.com',
-                          style: TextStyle(
-                              color: Colors.blue,
-                              decoration: TextDecoration.underline),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              // ignore: deprecated_member_use
-                              launch('mailto:synergypnrrvv@gmail.com');
-                            })
-                    ]))
-          ],
+                  text:
+                      '''Give your valuable feedbacks and suggestion on this email:  ''',
+                  style: TextStyle(color: Colors.black, fontSize: 15),
+                  children: [
+                    TextSpan(
+                      text: 'synergypnrrvv@gmail.com',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          // ignore: deprecated_member_use
+                          launch('mailto:synergypnrrvv@gmail.com');
+                        },
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+        Positioned(
+          bottom: 20,
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Center(
+              child: SizedBox(
+                height: 100,
+                width: 100,
+                child: Image(image: AssetImage('assets/SYNERGY LOGO.png')),
+              ),
+            ),
+          ),
+        )
+      ]),
     ));
   }
 }
